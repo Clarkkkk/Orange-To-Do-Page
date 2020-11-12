f69b536e7133861760eb7a1a0faa1ad5f5ab498b
Clark <Clark1729@outlook.com>
2020-11-12 19:14:32 +0800

    deploy

---
+++ precache-manifest.adc140157d17f16c5c173b994263fa2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337ba0805b53903789c0",
+    "url": "/Orange-To-Do-Page/css/app.ecdfd6d4.css"
+  },
+  {
+    "revision": "19da3a77822a40b5bc6e01e106afe2ae",
+    "url": "/Orange-To-Do-Page/favicon.ico"
+  },
+  {
+    "revision": "97ea58eeb63d4ad314ce2292de3e1bab",
+    "url": "/Orange-To-Do-Page/index.html"
+  },
+  {
+    "revision": "337ba0805b53903789c0",
+    "url": "/Orange-To-Do-Page/js/app.cba137e8.js"
+  },
+  {
+    "revision": "eeff686a6dc0046c66ae",
+    "url": "/Orange-To-Do-Page/js/chunk-vendors.aae04f7d.js"
+  },
+  {
+    "revision": "9695179a8ebe598324a09c9de37f0072",
+    "url": "/Orange-To-Do-Page/manifest.json"
+  },
+  {
+    "revision": "4ac0cc8c296b0157d160b83d19f69023",
+    "url": "/Orange-To-Do-Page/public/manifest.json"
+  }
+]);
